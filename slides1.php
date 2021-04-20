4aafaf70967173158ed422683085ca54ca3e0ae5
Ted OBrien <obrien_ted@hotmail.com>
2021-04-20 11:59:33 -0500

    slide show html

---
+++ slides1.php
@@ -0,0 +1,68 @@
+<?php
+
+
+
+?>
+
+<div id="audio-slideshow" class="audio-slideshow" data-audio="audio/dg1.mp3" data-audio-duration="235">
+<div class="audio-slides" id="slidedeck">
+<img name="" src="quotes/shows/gm1.jpg" data-slide-time="0">
+
+<img src="quotes/shows/Emma_Goldman0.jpg" name="Emma Goldman" height="400" align="middle" data-slide-time="4">
+<img src="quotes/shows/Emma_Goldman1.jpg" name="" height="400" align="middle" data-slide-time="9">
+<img src="quotes/shows/Emma_Goldman2.jpg" name="" height="345" align="middle" data-slide-time="15">
+<img src="quotes/shows/Emma_Goldman3.jpg" name="" height="400" align="middle" data-slide-time="20">
+<img src="quotes/shows/Emma_Goldman6.jpg" name="" height="400" align="middle" data-slide-time="26">
+
+<img src="quotes/shows/Dwight_D_Eisenhower1.jpg" name="Dwight Eisenhower" height="400" align="middle" data-slide-time="31">
+<img src="quotes/Dwight_D_Eisenhower.jpg" name="" height="380" align="middle" data-slide-time="38">
+<img src="quotes/shows/Dwight_D_Eisenhower2.jpg" name="" height="400" align="middle" data-slide-time="44">
+<img src="quotes/shows/Dwight_D_Eisenhower3.jpg" name="" height="400" align="middle" data-slide-time="51">
+
+<img src="quotes/shows/W_Edwards_Deming2.jpg" name="W Edwards Deming" height="400" align="middle" data-slide-time="58">
+<img src="quotes/shows/W_Edwards_Deming1.jpg" name="" height="400" align="middle" data-slide-time="64">
+<img src="quotes/shows/W_Edwards_Deming3.jpg" name="" height="360" align="middle" data-slide-time="70">
+<img src="quotes/shows/W_Edwards_Deming4.jpg" name="" height="400" align="middle" data-slide-time="76">
+<img src="quotes/shows/W_Edwards_Deming5.jpg" name="" height="400" align="middle" data-slide-time="81">
+
+<img src="quotes/shows/Anais_Nin1.jpg" name="Anais Nin" height="400" align="middle" data-slide-time="87">
+<img src="quotes/shows/Anais_Nin.jpg" name="" height="400" align="middle" data-slide-time="93">
+<img src="quotes/shows/Anais_Nin2.jpg" name="" height="400" align="middle" data-slide-time="99">
+<img src="quotes/shows/Anais_Nin3.jpg" name="" height="400" align="middle" data-slide-time="105">
+
+<img src="quotes/shows/Nelson_Mandela1.jpg" name="Nelson Mandela" height="400" align="middle" data-slide-time="111">
+<img src="quotes/shows/Nelson_Mandela2.jpg" name="" height="400" align="middle" data-slide-time="116">
+<img src="quotes/shows/Nelson_Mandela3.jpg" name="" height="390" align="middle" data-slide-time="121">
+<img src="quotes/shows/Nelson_Mandela4.jpg" name="" height="400" align="middle" data-slide-time="128">
+
+<img src="quotes/Judy_Garland.jpg" name="Judy Garland" height="400" align="middle" data-slide-time="134">
+<img src="quotes/shows/Judy_Garland1.jpg" name="" height="400" align="middle" data-slide-time="138">
+<img src="quotes/shows/Judy_Garland2.jpg" name="" height="385" align="middle" data-slide-time="142">
+<img src="quotes/shows/Judy_Garland6.jpg" name="" height="400" align="middle" data-slide-time="146">
+<img src="quotes/shows/Judy_Garland5.jpg" name="" height="400" align="middle" data-slide-time="152.6">
+
+<img src="quotes/shows/Bruce_Lee.jpg" name="Bruce Lee" height="400" align="middle" data-slide-time="158">
+<img src="quotes/shows/Bruce_Lee2.jpg" name="" height="400" align="middle" data-slide-time="164">
+<img src="quotes/shows/Bruce_Lee1.jpg" name="" height="390" align="middle" data-slide-time="170">
+<img src="quotes/shows/Bruce_Lee3.jpg" name="" height="400" align="middle" data-slide-time="176">
+<img src="quotes/shows/Bruce_Lee4.jpg" name="" height="400" align="middle" data-slide-time="182">
+
+<img src="quotes/shows/Bob_Dylan1.jpg" name="Bob Dylan" height="400" align="middle" data-slide-time="187">
+<img src="quotes/shows/Bob_Dylan2.jpg" name="" height="400" align="middle" data-slide-time="192">
+<img src="quotes/Bob_Dylan.jpg" name="" height="400" align="middle" data-slide-time="197">
+<img src="quotes/shows/Bob_Dylan3.jpg" name="" height="400" align="middle" data-slide-time="202">
+<img src="quotes/shows/Bob_Dylan4.jpg" name="" height="400" align="middle" data-slide-time="207">
+
+<img src="quotes/shows/Wayne_Gretzky4.jpg" name="Wayne Gretzky" height="400" align="middle" data-slide-time="211">
+<img src="quotes/shows/Wayne_Gretzky1.jpg" name="" height="400" align="middle" data-slide-time="216">
+<img src="quotes/shows/Wayne_Gretzky3.jpg" name="" height="400" align="middle" data-slide-time="220">
+<img src="quotes/shows/Wayne_Gretzky2.jpg" name="" height="400" align="middle" data-slide-time="225">
+<img src="quotes/Wayne_Gretzky.jpg" name="" height="345" align="middle" data-slide-time="230">
+
+<img src="quotes/shows/gm1.jpg" name="" data-slide-time="234">
+</div>
+<?php
+
+
+
+?>
